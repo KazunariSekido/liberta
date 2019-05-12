0e11b1089b86919f1ec1ba038cd597fcfc699d9c
Sekido Kazunari <aonceinalifetimechance0721@gmail.com>
2019-05-12 11:34:05 +0900

    topFaq,ツリー表示の実装

--- src/components/topcomp/topFaq.vue
+++ src/components/topcomp/topFaq.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topFaq">
     <h2>よくある質問</h2>
+    <el-tree :data="q1" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree :data="q2" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree :data="q3" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree :data="q4" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
     <input class="hd_btn" type="button" @click="toFaq" value="すべての質問を見る">
   </div>
 </template>
@@ -10,11 +14,42 @@ export default {
   name: 'TopFaq',
   data () {
     return {
+      q1: [{
+        label: 'Q.未経験・無資格でも大丈夫ですか？',
+        children: [{
+          label: 'A.Of course!!!'
+        }]
+      }],
+      q2: [{
+        label: 'Q.派遣社員に有給はありますか？',
+        children: [{
+          label: 'A.Of course!!!'
+        }]
+      }],
+      q3: [{
+        label: 'Q.ブランクが長いのですが、就職できますか？',
+        children: [{
+          label: 'A.Of course!!!'
+        }]
+      }],
+      q4: [{
+        label: 'Q.？？？？？？？',
+        children: [{
+          label: 'A.Of course!!!'
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
     }
   },
   methods: {
     toFaq () {
       this.$router.push('/faq')
+    },
+    handleNodeClick (data) {
+      console.log(data)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -8,8 +8,12 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 library.add(fas, fab, far)
+
+Vue.use(ElementUI)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
